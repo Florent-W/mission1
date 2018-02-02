f74cca104abdb879e02901936bac35f80678ea69
Florent <florent.weltmann@gmail.com>
2018-02-02 10:27:28 +0100

    Première version de la page de validation de la fiche de frais.

---
+++ controleurs/c_validerFrais.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Gestion des frais
+ *
+ * PHP Version 7
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Florent WELTMANN <florent.weltmann@gmail.com>
+ * @version   GIT: <0>
+ */
+
+$idVisiteur = $_SESSION['idVisiteur'];
+$mois = getMois(date('d/m/Y'));
+$numAnnee = substr($mois, 0, 4);
+$numMois = substr($mois, 4, 2);
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+?>
